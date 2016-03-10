fc363dde4419bb3abc79ff7352eb44aecf8295ff
tison <ts@ajrice.com>
2016-03-10 16:06:00 +0800

    也不知道是什么时候的版本了，反正这个是能找到的最新版本了

--- Application/Apiwiki/Conf/config.php
+++ Application/Apiwiki/Conf/config.php
@@ -2,10 +2,10 @@
 return array(
     /* 数据库配置 */
     'DB_TYPE'   => 'mysql',         // 数据库类型
-    'DB_HOST'   => '10.169.91.32',     // 服务器地址
+    'DB_HOST'   => 'localhost',     // 服务器地址
     'DB_NAME'   => 'apiwiki',       // 数据库名
-    'DB_USER'   => 'admin',              // 用户名
-    'DB_PWD'    => 'ajrice123ajrice123#',              // 密码
+    'DB_USER'   => 'root',              // 用户名
+    'DB_PWD'    => 'AJRice123#',              // 密码
     'DB_PORT'   => '3306',          // 端口
     'DB_PREFIX' => 'aw_',           // 数据库表前缀
 
